a3b224a206b5e9937676faad91df999a1c0f5e3d
dincengincan <engincandinc@gmail.com>
2020-08-16 17:10:57 +0300

    Initial commit

---
+++ sketch.js
@@ -0,0 +1,9 @@
+// eslint-disable-next-line no-unused-vars
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+// eslint-disable-next-line no-unused-vars
+function draw() {
+  background(255);
+}
